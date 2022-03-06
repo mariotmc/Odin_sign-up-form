1946ec8338db65477752c06c3bc9e5537385da70
mariotmc <mariotomicx@gmail.com>
2022-03-06 21:45:28 +0100

    Create content component

---
+++ src/components/Content.js
@@ -0,0 +1,15 @@
+import Info from "./Info";
+import Form from "./Form";
+import SignUp from "./SignUp";
+
+const Content = () => {
+  return (
+    <main className="content-container">
+      <Info />
+      <Form />
+      <SignUp />
+    </main>
+  );
+};
+
+export default Content;
