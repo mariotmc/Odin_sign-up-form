92b5ade13dfc7e859a24712275fd189f25d50dd4
mariotmc <mariotomicx@gmail.com>
2022-03-06 21:45:49 +0100

    Create info component

---
+++ src/components/Info.js
@@ -0,0 +1,18 @@
+const Info = () => {
+  return (
+    <section className="info">
+      <p className="info-text">
+        This is not a real online service! You know you need something like this in your life to help you
+        realize your deepest dreams.
+      </p>
+      <p className="info-text">
+        Sign up <em>now</em> to get started.
+      </p>
+      <p className="info-text">
+        You <em>know</em> you want to.
+      </p>
+    </section>
+  );
+};
+
+export default Info;
